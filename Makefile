7c262fc28b5cf9f4b87c7cde5e6e9d033156df8f
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2017-11-25 10:36:35 -0300

    agregada libreria, validacion input funcion

--- Makefile
+++ Makefile
@@ -2,20 +2,21 @@ JFLAGS = -d
 JC = javac
 JVM = java 
 OUT = ./out/
+LIB = './out/exp4j-0.4.8.jar'
 
 default:
 	$(JC) $(JFLAGS) $(OUT) ./src/matrices/Matrices.java
 	$(JC) $(JFLAGS) $(OUT) ./src/granja/Granja.java
-	$(JC) $(JFLAGS) $(OUT) ./src/funciones/Funciones.java
+	$(JC) $(JFLAGS) $(OUT) -cp .:$(LIB) ./src/funciones/Funciones.java
 
 clean:
 	$(RM) $(OUT)*.class
 
 Funciones:
-	$(JVM) -cp $(OUT) Funciones
+	$(JVM) -cp .:$(OUT):$(LIB) Funciones
 
 funciones:
-	$(JVM) -cp $(OUT) Funciones
+	$(JVM) -cp .:$(OUT):$(LIB) Funciones
 
 Matrices:
 	$(JVM) -cp $(OUT) Matrices
--- src/funciones/Funciones.java
+++ src/funciones/Funciones.java
@@ -1,6 +1,9 @@
 import java.util.*;
 import java.io.*;
 import java.util.concurrent.locks.*;
+import javax.script.*;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 class Funciones {
 	public static Lock lock = new ReentrantLock();
@@ -58,15 +61,108 @@ class Funciones {
 			String f = entrada.getValue();
 			System.out.println(l+" = "+f);
 		}
+
+		String evaluar = "5+3*2+1+2*5+1";
+		/*ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("js");
+		try {
+			Object result = engine.eval(evaluar);
+			System.out.println("resultado = "+result);
+		}
+		catch (ScriptException e) {
+			System.out.println("Error haciendo la operacion");
+			System.exit(-1);
+		}*/
+
+		Expression e = new ExpressionBuilder(evaluar).build();
+		double result = e.evaluate();
+		System.out.println("resultado = "+(int) result);
+
 		//////////////////////////////////////////*/
+
+		System.out.println("\nFunciones ingresadas!");
+		while (true) {
+			// Recibir input
+			System.out.println("\nIngrese operación: (o escriba 'q' para salir)");
+			Scanner reader = new Scanner(System.in);
+			String funcion = reader.nextLine().trim();
+
+			// Salir
+			if (funcion.compareTo("q") == 0) {
+				reader.close();
+				break;
+			}
+
+			// Buscar función recibida
+			String letra = funcion.substring(0,1);
+			if (!funciones.containsKey(letra+"(x)")) {
+				System.out.println("Función inválida, por favor ingrésela nuevamente.");
+				continue;
+			}
+			String num = funcion.substring(2, funcion.length()-1);
+			if (!isNumeric(num)) {
+				System.out.println("Función inválida, por favor ingrésela nuevamente.");
+				continue;
+			}
+
+			// Función válida
+			String value = funciones.get(funcion+"(x)");
+			System.out.println("F: "+funcion);
+			System.out.println("num: "+num);
+			RunFuncion calcular = new RunFuncion("Thread principal", value);
+
+		}
+
+		System.out.println("Saliendo.");
+
+
+		
+
+
 
 		
 
 	}
+
+	public static boolean isNumeric(String str) {
+  		try  {  
+    		double d = Double.parseDouble(str);  
+  		}  
+  		catch(NumberFormatException nfe)  {  
+    		return false;  
+  		}  
+  		return true;  
+	}
 }
 
 class RunFuncion implements Runnable {
+	private Thread t;
+   	private String threadName;
+   	private volatile boolean exit;
+   	private String operacion;
+   	private int resultado;
+
+   	// Constructor con condiciones iniciales
+   	RunFuncion(String name, String op) {
+   		threadName = name;
+   		exit = false;
+   		resultado = 0;
+   		operacion = op;
+   	}
 
 	public void run() {
 	}
+
+	// Correr el thread
+	public void start() {
+      if (t == null) {
+         t = new Thread (this, threadName);
+         t.start ();
+      }
+   	}
+
+   	// Detener el thread
+   	public void stop() {
+   		exit = true;
+   	}
 }
